cca517ebf67640a275d8735f596180bf1c76c14f
matvey <Cucumento@gmail.com>
2023-08-01 16:40:26 +0300

    Bitcoin value display with API integrated.

--- vue/src/components/TheWelcome.vue
+++ vue/src/components/TheWelcome.vue
@@ -1,16 +1,45 @@
 <script setup>
-import WelcomeItem from './WelcomeItem.vue'
+import WelcomeItem from '@/components/WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import {onMounted} from "vue";
+import {ref} from 'vue'
+import axios from "axios";
+
+let info = ref(null)
+let loading = ref(true)
+let errored = ref(false)
+
+function initialiseBitcoinData() {
+  axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        info.value = response.data.bpi;
+      })
+      .catch(error => {
+        console.log(error);
+        errored.value = true;
+      })
+      .finally(() => (loading.value = false));
+}
+
+function currencyDecimal(value) {
+  return value.toFixed(2)
+}
+
+onMounted(() => {
+  initialiseBitcoinData()
+})
+
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <DocumentationIcon/>
     </template>
     <template #heading>Documentation</template>
 
@@ -21,7 +50,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <ToolingIcon/>
     </template>
     <template #heading>Tooling</template>
 
@@ -34,14 +63,14 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
     <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
 
-    <br />
+    <br/>
 
     More instructions are available in <code>README.md</code>.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <EcosystemIcon/>
     </template>
     <template #heading>Ecosystem</template>
 
@@ -57,7 +86,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
+      <CommunityIcon/>
     </template>
     <template #heading>Community</template>
 
@@ -65,7 +94,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
     Discord server, or
     <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
+    >StackOverflow</a
     >. You should also subscribe to
     <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
     the official
@@ -75,7 +104,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
+      <SupportIcon/>
     </template>
     <template #heading>Support Vue</template>
 
@@ -83,4 +112,26 @@ import SupportIcon from './icons/IconSupport.vue'
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon/>
+    </template>
+    <template #heading>Test API</template>
+    <div v-if="loading">Loading...</div>
+    <div
+        v-else
+        v-for="currency in info"
+        class="currency"
+    >
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currencyDecimal(currency.rate_float )}}
+      </span>
+    </div>
+  </WelcomeItem>
 </template>
+
+
+
+
